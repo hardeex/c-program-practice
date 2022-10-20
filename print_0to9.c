0026b7ecf7108d575a89c961fed54e6d0b16f00c
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-20 17:45:11 +0100

    learning nested function

---
+++ print_0to9.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void print_numbers(void): A function that prints number from 0 to 9
+ * followed by a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{                       
+        int number;
+        for(number = 0; number <=9; number++)
+        {       
+                _putchar(number);
+                }
+
+                _putchar('\n');
+}
+                      
---
+++ q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+    printf("%c", i);
+}
+
+printf("Question 1 \n");
+for (i = 0; i < 10; i++)
+{
+    printf("%d", i * 2);
+}
+printf("\n");
+
+}
